87628b6115a93580ce6bf5a82cc403c251acd617
KaindlJulian <kaindl745@gmail.com>
2019-02-15 22:28:00 +0100

    Add styling :nail_care:

--- web/index.js
+++ web/index.js
@@ -1,4 +1,6 @@
 import * as wasm from "mandelbrot-wasm";
 //import * as wasm from "@juliankaindl/mandelbrot-wasm";
 
-wasm.greet("from js to wasm and back")
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+wasm.draw(ctx, 600, 600, -0.15, 0.65);
