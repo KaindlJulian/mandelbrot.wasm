79345b74252274584ba52632f807674fedfb1f6f
KaindlJulian <kaindl745@gmail.com>
2019-02-15 20:59:14 +0100

    :spider_web: Update Webpack

--- web/index.js
+++ web/index.js
@@ -1,3 +1,4 @@
 import * as wasm from "mandelbrot-wasm";
-//import * as wasm from "hello-wasm-pack";
-wasm.greet("s")
+//import * as wasm from "@juliankaindl/mandelbrot-wasm";
+
+wasm.greet("from js to wasm and back")
