11c81f8ab4dc5adc25ded6101c2acca92d4defdc
KaindlJulian <kaindl745@gmail.com>
2019-02-25 16:48:17 +0100

    Add basic UI

--- web/index.js
+++ web/index.js
@@ -4,4 +4,6 @@ import * as wasm from "mandelbrot-wasm";
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+console.time('draw');
 wasm.draw(ctx, canvas.scrollWidth, canvas.scrollHeight, 0.25001, 0.0);
+console.timeEnd('draw');
