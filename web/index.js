c520a23a2d6d25b66ebb11fcfe25842bec58cd8a
KaindlJulian <kaindl745@gmail.com>
2019-03-07 02:12:53 +0100

    Add controls to real, imaginary and zoom

--- web/index.js
+++ web/index.js
@@ -1,9 +1,40 @@
 import * as wasm from "mandelbrot-wasm";
 //import * as wasm from "@juliankaindl/mandelbrot-wasm";
 
+// initial drawing
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+let currentZoom = 1.0;
+wasm.draw(ctx, canvas.scrollWidth, canvas.scrollHeight, 0.0, 0.0, currentZoom);
 
-console.time('draw');
-wasm.draw(ctx, canvas.scrollWidth, canvas.scrollHeight, 0.25001, 0.0);
-console.timeEnd('draw');
+// on apply
+document.getElementById('apply-btn').addEventListener('click', _ => {
+    const real = parseFloat(document.getElementById('real').value);
+    const imaginary = parseFloat(document.getElementById('imaginary').value);
+    wasm.draw(ctx, canvas.scrollWidth, canvas.scrollHeight, real, imaginary, currentZoom);
+});
+
+// on reset
+document.getElementById('reset-btn').addEventListener('click', _ => {
+    currentZoom = 1.0;
+    wasm.draw(ctx, canvas.scrollWidth, canvas.scrollHeight, 0.0, 0.0, currentZoom);
+});
+
+// on zoom in 
+document.getElementById('zoom-in').addEventListener('click', _ => {
+    currentZoom++;
+    const real = parseFloat(document.getElementById('real').value);
+    const imaginary = parseFloat(document.getElementById('imaginary').value);
+    wasm.draw(ctx, canvas.scrollWidth, canvas.scrollHeight, real, imaginary, currentZoom);
+});
+
+// on zoom out
+document.getElementById('zoom-out').addEventListener('click', _ => {
+    if (currentZoom >= 2) {
+        currentZoom--;
+        const real = parseFloat(document.getElementById('real').value);
+        const imaginary = parseFloat(document.getElementById('imaginary').value);
+        wasm.draw(ctx, canvas.scrollWidth, canvas.scrollHeight, real, imaginary, currentZoom);
+    }
+});
+
